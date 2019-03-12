cc0f64bcf1358c8244835e64c3589273567592b1
Woner1 <lzzengyu@163.com>
2019-03-12 17:07:54 +0800

    activeadmin

--- config/application.rb
+++ config/application.rb
@@ -17,15 +17,20 @@ require "rails/test_unit/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
 module Qingqiyichu
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+
+
     config.middleware.use Rack::MethodOverride
     config.middleware.use ActionDispatch::Flash
     config.middleware.use ActionDispatch::Cookies
     config.middleware.use ActionDispatch::Session::CookieStore
+
+    config.app = config_for(:app).with_indifferent_access
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
---
+++ db/migrate/20190312081703_create_vlogs.rb
@@ -0,0 +1,10 @@
+class CreateVlogs < ActiveRecord::Migration[5.2]
+  def change
+    create_table :vlogs do |t|
+      t.string :picture
+      t.string :content
+
+      t.timestamps
+    end
+  end
+end
